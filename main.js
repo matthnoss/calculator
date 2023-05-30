cf3fa38f06f30725871d047b088434f9647a275c
matthnoss <davidgrgaca@icloud.com>
2023-05-30 15:37:27 +0200

    Added operation functionality, support for multiple calculations at once

--- main.js
+++ main.js
@@ -35,3 +35,124 @@ function operate(firstNumber, secondNumber, operator) {
     }
     return result;
 }
+
+//onclick
+
+const buttons = document.querySelectorAll('.btn');
+const display = document.getElementById('calcDisplay');
+let firstNumber = 0;
+let secondNumber = 0;
+let operation;
+
+buttons.forEach((currentButton) => {
+    currentButton.addEventListener('click', () => {
+        typeNumber(currentButton)})
+})
+
+// function typeNumber(currentButton) {
+//     if(display.textContent === '0') {
+//         clearDisplay();
+//     }
+//     display.textContent += currentButton.textContent;
+// }
+
+function typeNumber(currentButton) {
+    if(display.textContent === '0' && currentButton.textContent === '.') {
+        display.textContent += currentButton.textContent;
+    } else if (display.textContent === '0') {
+        clearDisplay();
+        display.textContent += currentButton.textContent;
+    } else {
+        display.textContent += currentButton.textContent;
+    }
+    
+}
+
+function clearDisplay(input) {
+    if (input === null) {
+        display.textContent = '';
+    } else {
+        display.textContent = input;
+    }
+    
+}
+
+//clear button functionality
+const clearButton = document.querySelector('.clearBtn')
+
+clearButton.addEventListener('click', () => {
+    clearDisplay(0);
+    firstNumber = '0';
+    secondNumber = '0';
+})
+
+
+//operation buttons
+const operationButtons = document.querySelectorAll('.operationBtn');
+
+operationButtons.forEach((currentButton) => {
+    currentButton.addEventListener('click', () => {
+        operationButtonPressed(currentButton);
+    })
+})
+
+let lastOperation = '0';
+
+//operating function
+function operationButtonPressed(input) {
+    lastOperation = operation;
+    operation = input.textContent;
+
+    if(lastOperation == '0') {
+        lastOperation = input.textContent;
+    }
+    let result;
+    
+
+    if (firstNumber == '0'){
+        firstNumber = display.textContent;
+        clearDisplay('0');
+    } else if (secondNumber == '0') {
+        if (firstNumber != '0' && display.textContent != '0') {
+            secondNumber = display.textContent;
+            clearDisplay('0');
+        } else {
+            firstNumber = display.textContent;
+            clearDisplay('0');
+        }
+    } 
+
+    if (firstNumber != '0' && secondNumber != '0') {
+        console.log('cond.')
+        result = operate(firstNumber, secondNumber, lastOperation);
+        console.log(result);
+        firstNumber = result;
+        secondNumber = '0';
+    }
+
+}
+
+//equals button
+
+const equalsButton = document.querySelector('.equals');
+
+equalsButton.addEventListener('click', () => {
+    secondNumber = display.textContent;
+    console.log(firstNumber);
+    console.log(secondNumber);
+    console.log(operation);
+
+    if (secondNumber == 0 && operation == '/') {
+        clearDisplay("Cannot divide by zero.");
+        firstNumber, secondNumber = '0';
+    } else if (firstNumber == '0' || secondNumber == '0') {
+
+    } else {
+        let result = operate(firstNumber, secondNumber, operation);
+        result = Number(result.toFixed(5));
+        clearDisplay(result);
+    }
+
+
+    
+})
