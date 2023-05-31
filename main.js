de06e3db3fd276d792b0fdfb61b17f3698e27278
matthnoss <davidgrgaca@icloud.com>
2023-05-31 19:42:13 +0200

    Reorganizing JS file

--- main.js
+++ main.js
@@ -36,6 +36,127 @@ function operate(firstNumber, secondNumber, operator) {
     return result;
 }
 
+function deleteCharacter() {
+    if (display.textContent !== '0') {
+        let text = display.textContent;
+        text = text.slice(0, -1);
+        display.textContent = text;
+    }
+    if (display.textContent.length === 0) {
+        clearDisplay('0');
+    }
+}
+
+function keyStrokeType(textContent) {
+    if (display.textContent.length < 16) {
+        if (display.textContent === '0' && textContent === '.') {
+            display.textContent += textContent;
+        } else if (display.textContent.includes('.') && textContent === '.') {
+
+        } else if (display.textContent === '0') {
+            clearDisplay();
+            display.textContent += textContent;
+        } else {
+            display.textContent += textContent;
+        }
+    }
+
+}
+
+function typeNumber(currentButton) {
+
+    if (display.textContent.length < 16) {
+        if (display.textContent === '0' && currentButton.textContent === '.') {
+            display.textContent += currentButton.textContent;
+        } else if (display.textContent.includes('.') && currentButton.textContent === '.') {
+
+        } else if (display.textContent === '0') {
+            clearDisplay();
+            display.textContent += currentButton.textContent;
+        } else {
+            display.textContent += currentButton.textContent;
+        }
+    }
+
+
+}
+
+function clearDisplay(input) {
+    if (input === null) {
+        display.textContent = '';
+    } else {
+        display.textContent = input;
+    }
+
+}
+
+function clearValues() {
+    clearDisplay(0);
+    firstNumber = '0';
+    secondNumber = '0';
+    operation = '0';
+    lastOperation = '0';
+}
+
+
+//operating function
+function operationButtonPressed(input) {
+    lastOperation = operation;
+
+    if (input.textContent == undefined) {
+        operation = input;
+    } else {
+        operation = input.textContent;
+    }
+
+    if (lastOperation == '0') {
+        lastOperation = input.textContent;
+    }
+    let result;
+
+
+    if (firstNumber == '0') {
+        firstNumber = display.textContent;
+        clearDisplay('0');
+    } else if (secondNumber == '0') {
+        if (firstNumber != '0' && display.textContent != '0') {
+            secondNumber = display.textContent;
+            clearDisplay('0');
+        } else {
+            firstNumber = display.textContent;
+            clearDisplay('0');
+        }
+    }
+
+    if (firstNumber != '0' && secondNumber != '0') {
+        console.log('cond.')
+        result = operate(firstNumber, secondNumber, lastOperation);
+        console.log(result);
+        firstNumber = result;
+        secondNumber = '0';
+    }
+
+}
+
+function equalsButtonFunc() {
+    secondNumber = display.textContent;
+    console.log(firstNumber);
+    console.log(secondNumber);
+    console.log(operation);
+
+    if (secondNumber == 0 && operation == '/') {
+        clearDisplay("Cannot divide by zero.");
+        firstNumber, secondNumber = '0';
+    } else if (firstNumber == '0' || secondNumber == '0') {
+
+    } else {
+        let result = operate(firstNumber, secondNumber, operation);
+        console.log(result);
+        result = Number(result.toFixed(5));
+        clearDisplay(result);
+    }
+}
+
 //onclick
 
 const buttons = document.querySelectorAll('.btn');
@@ -98,54 +219,13 @@ deleteButton.addEventListener('click', () => {
 })
 
 
-function deleteCharacter() {
-    if (display.textContent !== '0') {
-        let text = display.textContent;
-        text = text.slice(0, -1);
-        display.textContent = text;
-    }
-    if (display.textContent.length === 0) {
-        clearDisplay('0');
-    }
-}
+
 
 //function for entering numbers via keyboard
 
-function keyStrokeType(textContent) {
-    if (display.textContent === '0' && textContent === '.') {
-        display.textContent += textContent;
-    } else if (display.textContent.includes('.') && textContent === '.') {
 
-    } else if (display.textContent === '0') {
-        clearDisplay();
-        display.textContent += textContent;
-    } else {
-        display.textContent += textContent;
-    }
-}
 
-function typeNumber(currentButton) {
-    if (display.textContent === '0' && currentButton.textContent === '.') {
-        display.textContent += currentButton.textContent;
-    } else if (display.textContent.includes('.') && currentButton.textContent === '.') {
 
-    } else if (display.textContent === '0') {
-        clearDisplay();
-        display.textContent += currentButton.textContent;
-    } else {
-        display.textContent += currentButton.textContent;
-    }
-
-}
-
-function clearDisplay(input) {
-    if (input === null) {
-        display.textContent = '';
-    } else {
-        display.textContent = input;
-    }
-
-}
 
 //clear button functionality
 const clearButton = document.querySelector('.clearBtn')
@@ -154,13 +234,7 @@ clearButton.addEventListener('click', () => {
     clearValues();
 })
 
-function clearValues() {
-    clearDisplay(0);
-    firstNumber = '0';
-    secondNumber = '0';
-    operation = '0';
-    lastOperation = '0';
-}
+
 
 
 //operation buttons
@@ -174,44 +248,7 @@ operationButtons.forEach((currentButton) => {
 
 let lastOperation = '0';
 
-//operating function
-function operationButtonPressed(input) {
-    lastOperation = operation;
 
-    if (input.textContent == undefined) {
-        operation = input;
-    } else {
-        operation = input.textContent;
-    }
-
-    if (lastOperation == '0') {
-        lastOperation = input.textContent;
-    }
-    let result;
-
-
-    if (firstNumber == '0') {
-        firstNumber = display.textContent;
-        clearDisplay('0');
-    } else if (secondNumber == '0') {
-        if (firstNumber != '0' && display.textContent != '0') {
-            secondNumber = display.textContent;
-            clearDisplay('0');
-        } else {
-            firstNumber = display.textContent;
-            clearDisplay('0');
-        }
-    }
-
-    if (firstNumber != '0' && secondNumber != '0') {
-        console.log('cond.')
-        result = operate(firstNumber, secondNumber, lastOperation);
-        console.log(result);
-        firstNumber = result;
-        secondNumber = '0';
-    }
-
-}
 
 //equals button
 
@@ -222,21 +259,3 @@ equalsButton.addEventListener('click', () => {
 
 })
 
-function equalsButtonFunc() {
-    secondNumber = display.textContent;
-    console.log(firstNumber);
-    console.log(secondNumber);
-    console.log(operation);
-
-    if (secondNumber == 0 && operation == '/') {
-        clearDisplay("Cannot divide by zero.");
-        firstNumber, secondNumber = '0';
-    } else if (firstNumber == '0' || secondNumber == '0') {
-
-    } else {
-        let result = operate(firstNumber, secondNumber, operation);
-        console.log(result);
-        result = Number(result.toFixed(5));
-        clearDisplay(result);
-    }
-}
